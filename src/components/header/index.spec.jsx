359063e5e82c02da0a7a8f8e08174249bddac3ce
prasana shinde <prasana.shinde.rebase@gmail.com>
2022-02-14 16:48:23 +0530

    created header component

--- src/components/header/index.spec.jsx
+++ src/components/header/index.spec.jsx
@@ -1,10 +1,22 @@
 /* eslint-disable testing-library/render-result-naming-convention */
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { renderHeader } from './index';
+
+jest.spyOn(window, 'alert');
 
 test('renders header component successfully', () => {
   const Header = renderHeader();
   render(<Header />);
-  const textElement = screen.getByText(/header component/i);
-  expect(textElement).toBeInTheDocument();
+  const loginButton = screen.getByText(/login/i);
+  const moreButton = screen.getByText(/more/i);
+  const cartButton = screen.getByText(/cart/i);
+  expect(loginButton).toBeInTheDocument();
+  expect(moreButton).toBeInTheDocument();
+  expect(cartButton).toBeInTheDocument();
+  fireEvent.click(screen.getByTestId('login'));
+  expect(window.alert).toBeCalledWith('login clicked');
+  fireEvent.click(screen.getByTestId('more'));
+  expect(window.alert).toBeCalledWith('more clicked');
+  fireEvent.click(screen.getByTestId('cart'));
+  expect(window.alert).toBeCalledWith('cart clicked');
 });
