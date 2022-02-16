bc3e2da665c7df87ccf2181a1227b7e94ccf02c0
praveenrebase <88529751+praveenrebase@users.noreply.github.com>
2022-02-16 18:27:53 +0530

    redirect to cart ad home

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,14 @@
-import { useCartCount as defaultCartCount } from "../../customHooks/useCartCount"
+import { useCartCount as defaultCartCount } from "../../customHooks/useCartCount";
+import { Link } from 'react-router-dom';
 
 export function renderHeader(cartService, useCartCount = defaultCartCount) {
   return function Header() {
     const { cartCount } = useCartCount(cartService);
     return <>
       <div className="sticky top-0 z-50 bg-blue-500 h-16 flex justify-center text-white">
-        <div className="flex items-center pr-3">
+        <Link to="/" className="flex items-center pr-3">
           <img className="h-5" src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png" alt="logo" />
-        </div>
+        </Link>
         <div className="flex items-center pr-56">
           <div className="h-9 rounded-sm bg-white pt-1">
             <input className="text-sm w-96 text-black placeholder-slate-500 tracking-tighter text-left pl-2 outline-none" type="text" title="Search for products, brands and more" placeholder="Search for products, brands and more"/>
@@ -26,7 +27,7 @@ export function renderHeader(cartService, useCartCount = defaultCartCount) {
         </div>
         <div className="flex items-center pr-1">
         </div>
-        <div onClick={() => alert("cart clicked")} data-testid="cart" className="flex items-center">
+        <Link to="/checkout" className="flex items-center">
           {
             // If cartCount id zero do not show the count
             cartCount > 0 ? <div className="border relative text-xs left-5 px-1 rounded-full bg-orange-500 bottom-3">{cartCount}</div> : null
@@ -34,7 +35,7 @@ export function renderHeader(cartService, useCartCount = defaultCartCount) {
           
           <svg width="14" height="14" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M15.32 2.405H4.887C3 2.405 2.46.805 2.46.805L2.257.21C2.208.085 2.083 0 1.946 0H.336C.1 0-.064.24.024.46l.644 1.945L3.11 9.767c.047.137.175.23.32.23h8.418l-.493 1.958H3.768l.002.003c-.017 0-.033-.003-.05-.003-1.06 0-1.92.86-1.92 1.92s.86 1.92 1.92 1.92c.99 0 1.805-.75 1.91-1.712l5.55.076c.12.922.91 1.636 1.867 1.636 1.04 0 1.885-.844 1.885-1.885 0-.866-.584-1.593-1.38-1.814l2.423-8.832c.12-.433-.206-.86-.655-.86" fill="#fff"></path></svg>
           <span className="ml-2">Cart</span>
-        </div>
+        </Link>
         </div>
     </>
   }
