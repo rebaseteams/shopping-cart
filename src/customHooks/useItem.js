7a9d9e05495779524b8e01d15840296caf7a59cc
praveenrebase <88529751+praveenrebase@users.noreply.github.com>
2022-02-17 15:47:45 +0530

    added new products

--- src/customHooks/useItem.js
+++ src/customHooks/useItem.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 export function useItem (itemsService, provider) {
   const [product, setProduct] = useState();
   const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState('');
 
   const { id } = useParams();
 
@@ -13,11 +14,14 @@ export function useItem (itemsService, provider) {
   useEffect(() => {
     setLoading(true);
     const item = itemsService.getItem(id);
-    if (item) setProduct(item);
+    if (item) {
+      setProduct(item);
+      setImage(item.images[0]);
+    }
     setLoading(false);
-  }, [])
+  }, [id])
 
   return {
-    loading, product, setCount, value
+    loading, product, setCount, value, setImage, image
   }
 }
